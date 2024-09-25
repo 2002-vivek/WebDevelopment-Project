b155d2fd00ec352d5b5234aef981ea27840d1ad7
vivek <eegavivekreddy@gmail.com>
2024-09-24 23:33:09 -0400

    Header+flex grid completed

---
+++ script.js
@@ -0,0 +1,15 @@
+<script>
+        // JavaScript to toggle menu visibility on hamburger click
+        const hamburger = document.querySelector('.hamburger');
+        const menu = document.querySelector('.menu');
+
+        hamburger.addEventListener('click', () => {
+            menu.classList.toggle('active');
+        });
+
+
+
+
+
+        
+    </script>
